9f3f21488d907e914d704faec89c1582a4fb7994
yousef-bigdeli <yousefbigdeli96@gmail.com>
2022-07-12 19:00:40 +0430

    feat: Create breadcrumb component

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,8 +1,30 @@
-// import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
 
 const CategoryPage = () => {
+  const { pathname } = useLocation();
   // const { name, brand } = useParams();
-  return <></>;
+
+  const pathArray = pathname.split("/");
+  const crumbs = pathArray.map((item, index) => {
+    const path = pathArray
+      .slice(0, index + 1)
+      .reduce(
+        (prevPath, currentItem) =>
+          `${prevPath === "/" ? "" : prevPath}/${currentItem}`,
+        ""
+      );
+    const title = path === "/" ? "Home" : item.replace("-", " ");
+
+    return { path, title };
+  });
+
+  return (
+    <>
+      <Breadcrumb crumbs={crumbs} />
+    </>
+  );
 };
 
 export default CategoryPage;
