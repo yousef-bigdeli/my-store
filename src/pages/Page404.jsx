cb2386dc84e99aae91427ac750006a5a334eb477
yousef-bigdeli <yousefbigdeli96@gmail.com>
2022-08-01 16:43:56 +0430

    style: change 404 styles

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,10 @@
 const Page404 = () => {
-  return <div>404</div>;
+  return <div className='appContainer'>
+    <div className='text-center flex flex-col items-center justify-center' style={{height: '50vh'}}>
+      <div className='text-primaryColor' style={{fontSize: "72px"}}>404</div>
+      <p className='text-24'>OOPS! NOTHING WAS FOUND</p>
+    </div>
+  </div>;
 };
 
 export default Page404;
